e64924cd79ba92ae55fdb88ecdc7b450d0baf057
Wookie <bq1214@gmail.com>
2022-09-02 22:17:43 +0500

    Updated rarities

--- lua/nebulainv/items/case.lua
+++ lua/nebulainv/items/case.lua
@@ -35,88 +35,6 @@ NebulaInv:RegisterItem("case", "nades", {
     }
 })
 
--- Rarity 3
-
-NebulaInv:RegisterItem("case", "red_woopie", {
-    rarity = 3,
-    imgur = "CVtvhB5",
-    name = "Blood Moon Collection",
-    items = {
-        ["weapon_tfa_cso_xm1014red"] = 40,
-        ["weapon_tfa_cso_awp_red"] = 30,
-        ["weapon_tfa_cso_balrog1"] = 10,
-        ["weapon_tfa_cso_balrog3"] = 10,
-        ["weapon_tfa_cso_balrog5"] = 5,
-        ["weapon_tfa_cso_balrog7"] = 4,
-        ["weapon_tfa_cso_balrog11"] = 1,
-    }
-})
-
-NebulaInv:RegisterItem("case", "green_lego", {
-    rarity = 3,
-    imgur = "yIrEscj",
-    name = "Green Lego Collection",
-    items = {
-        ["weapon_tfa_cso_m777"] = 20,
-        ["weapon_tfa_cso_s1451"] = 20,
-        ["weapon_tfa_cso_katyusha"] = 15,
-        ["weapon_tfa_cso_t50"] = 15,
-        ["weapon_tfa_cso_howitzer"] = 10,
-        ["weapon_tfa_cso_tank"] = 10,
-        ["weapon_tfa_cso_brickpiecev2"] = 5,
-        ["weapon_tfa_cso_v2rocket"] = 5,
-    }
-})
-
--- Rarity 4
-
-NebulaInv:RegisterItem("case", "dual", {
-    rarity = 4,
-    imgur = "dkKVhtk",
-    name = "Dual Collection I",
-    items = {
-        ["weapon_tfa_cso_dualinfinity"] = 25,
-        ["weapon_tfa_cso_dualuzi"] = 20,
-        ["weapon_tfa_cso_dualuzi_v6"] = 20,
-        ["weapon_tfa_cso_dmp7a1"] = 15,
-        ["weapon_tfa_cso_dualinfinityfinal"] = 10,
-        ["weapon_tfa_cso_gunkata"] = 5,
-        ["weapon_tfa_cso_rainbowkata"] = 3,
-        ["weapon_tfa_cso_rainbowkata"] = 2,
-    }
-})
-
--- Rarity 5
-
-NebulaInv:RegisterItem("case", "vulcan", {
-    rarity = 5,
-    imgur = "yzVzyRg",
-    name = "Vulcanus Collection",
-    items = {
-        ["weapon_tfa_cso_vulcanus11"] = 40,
-        ["weapon_tfa_cso_vulcanus3"] = 30,
-        ["weapon_tfa_cso_vulcanus5"] = 20,
-        ["weapon_tfa_cso_vulcanus7"] = 10,
-    }
-})
-
-NebulaInv:RegisterItem("case", "tactical", {
-    rarity = 1,
-    imgur = "HAEu4Lv",
-    name = "Tactical Case",
-    items = {
-        ["weapon_luger_expert"] = 25,
-        ["weapon_tomahawk"] = 20,
-        ["weapon_p90x"] = 20,
-        ["weapon_scar_ozce"] = 15,
-        ["weapon_tfa_cso_vulcanus3"] = 10,
-        ["suit_camoguy"] = 5,
-        ["suit_confusion"] = 5,
-    }
-})
-
--- Rarity 6 (CASES CAN'T BE PERM!)
-
 NebulaInv:RegisterItem("case", "cosmetic1", {
     rarity = 2,
     imgur = "h2GzNYj",
@@ -149,6 +67,39 @@ NebulaInv:RegisterItem("case", "cosmetic2", {
         ["accessory_venturioncape"] = 10,
         ["accessory_waveform"] = 10,
         ["accessory_streetracer"] = 5,
+    }
+})
+
+-- Rarity 3
+
+NebulaInv:RegisterItem("case", "red_woopie", {
+    rarity = 3,
+    imgur = "CVtvhB5",
+    name = "Blood Moon Collection",
+    items = {
+        ["weapon_tfa_cso_xm1014red"] = 40,
+        ["weapon_tfa_cso_awp_red"] = 30,
+        ["weapon_tfa_cso_balrog1"] = 10,
+        ["weapon_tfa_cso_balrog3"] = 10,
+        ["weapon_tfa_cso_balrog5"] = 5,
+        ["weapon_tfa_cso_balrog7"] = 4,
+        ["weapon_tfa_cso_balrog11"] = 1,
+    }
+})
+
+NebulaInv:RegisterItem("case", "green_lego", {
+    rarity = 3,
+    imgur = "yIrEscj",
+    name = "Green Lego Collection",
+    items = {
+        ["weapon_tfa_cso_m777"] = 20,
+        ["weapon_tfa_cso_s1451"] = 20,
+        ["weapon_tfa_cso_katyusha"] = 15,
+        ["weapon_tfa_cso_t50"] = 15,
+        ["weapon_tfa_cso_howitzer"] = 10,
+        ["weapon_tfa_cso_tank"] = 10,
+        ["weapon_tfa_cso_brickpiecev2"] = 5,
+        ["weapon_tfa_cso_v2rocket"] = 5,
     }
 })
 
@@ -186,6 +137,24 @@ NebulaInv:RegisterItem("case", "cosmetic4", {
     }
 })
 
+-- Rarity 4
+
+NebulaInv:RegisterItem("case", "dual", {
+    rarity = 4,
+    imgur = "dkKVhtk",
+    name = "Dual Collection I",
+    items = {
+        ["weapon_tfa_cso_dualinfinity"] = 25,
+        ["weapon_tfa_cso_dualuzi"] = 20,
+        ["weapon_tfa_cso_dualuzi_v6"] = 20,
+        ["weapon_tfa_cso_dmp7a1"] = 15,
+        ["weapon_tfa_cso_dualinfinityfinal"] = 10,
+        ["weapon_tfa_cso_gunkata"] = 5,
+        ["weapon_tfa_cso_rainbowkata"] = 3,
+        ["weapon_tfa_cso_rainbowkata"] = 2,
+    }
+})
+
 NebulaInv:RegisterItem("case", "cosmetic5", {
     rarity = 4,
     imgur = "rh87y3V",
@@ -203,6 +172,35 @@ NebulaInv:RegisterItem("case", "cosmetic5", {
     }
 })
 
+-- Rarity 5
+
+NebulaInv:RegisterItem("case", "vulcan", {
+    rarity = 5,
+    imgur = "yzVzyRg",
+    name = "Vulcanus Collection",
+    items = {
+        ["weapon_tfa_cso_vulcanus11"] = 40,
+        ["weapon_tfa_cso_vulcanus3"] = 30,
+        ["weapon_tfa_cso_vulcanus5"] = 20,
+        ["weapon_tfa_cso_vulcanus7"] = 10,
+    }
+})
+
+NebulaInv:RegisterItem("case", "tactical", {
+    rarity = 5,
+    imgur = "HAEu4Lv",
+    name = "Tactical Case",
+    items = {
+        ["weapon_luger_expert"] = 25,
+        ["weapon_tomahawk"] = 20,
+        ["weapon_p90x"] = 20,
+        ["weapon_scar_ozce"] = 15,
+        ["weapon_tfa_cso_vulcanus3"] = 10,
+        ["suit_camoguy"] = 5,
+        ["suit_confusion"] = 5,
+    }
+})
+
 NebulaInv:RegisterItem("case", "suits1", {
     rarity = 5,
     imgur = "wdqRSk9",
@@ -217,6 +215,8 @@ NebulaInv:RegisterItem("case", "suits1", {
         ["suit_dragonfly"] = 2,
     }
 })
+
+-- Rarity 6 (CASES CAN'T BE PERM!)
 
 /*
 local items = table.Copy(NebulaInv.Items)
