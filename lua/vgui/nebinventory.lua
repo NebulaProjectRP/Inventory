a3c53e1c12a5b7e11def3afecfe268f430ad2111
Wookie <bq1214@gmail.com>
2022-09-05 15:44:44 +0500

    Formatting code

--- lua/vgui/nebinventory.lua
+++ lua/vgui/nebinventory.lua
@@ -641,13 +641,14 @@ end)
 
 net.Receive("Nebula.Inv:RemoveEquipment", function(l, ply)
     local empty = net.ReadString()
+
     for k, v in pairs(NebulaInv.Loadout or {}) do
         if empty ~= k and not string.StartWith(k, "weapon:") then continue end
         local item = NebulaInv.Items[v.id]
 
-        if item.rarity < 6 then
-            NebulaInv.Loadout[k] = nil
-        end
+        if item.rarity >= 6 then continue end
+
+        NebulaInv.Loadout[k] = nil
     end
 end)
 
